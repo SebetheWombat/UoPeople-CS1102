220f97a55a8bdc2a85ca580de4b14632291e7a9e
Sebe Keller <sebethkeller@gmail.com>
2017-05-09 20:08:01 -0400

    Started OOP with payroll systems

---
+++ PayrollSystem/src/payrollsystem/Employee.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payrollsystem;
+
+/**
+ *
+ * @author Sebe
+ */
+public class Employee {
+    private int empID;
+    private String name;
+    private Vehicle vehicle;
+
+    /**
+     * @return the empID
+     */
+    public int getEmpID() {
+        return empID;
+    }
+
+    /**
+     * @param empID the empID to set
+     */
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the vehicle
+     */
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    /**
+     * @param vehicle the vehicle to set
+     */
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+    
+}
